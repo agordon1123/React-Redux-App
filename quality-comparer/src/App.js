f09a3ff170acdea9c6bb5629a73adb63ed91ff7d
Alex <agordon1123@gmail.com>
2019-08-15 15:34:08 -0400

    dependencies installed and app laid out

---
+++ quality-comparer/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import CityList from './components/CityList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello from App</h1>
+      <CityList />
+    </div>
+  );
+}
+
+export default App;
---
+++ quality-comparer/src/components/CityList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import City from './City';
+
+const CityList = () => {
+    return (
+        <div>
+            <h1>Hello from CityList</h1>
+            <City />
+        </div>
+    )
+}
+
+export default CityList;
